6fd47450cde13d1fab0ee2ecb8ba238f030a9846
sar287 <sar287@txstate.edu>
2024-11-21 15:24:18 -0600

    completed report

--- Test/detection_api.py
+++ Test/detection_api.py
@@ -7,23 +7,14 @@ app = Flask(__name__)
 
 # Sample data
 sample_data = [
-    [{'depth': 0.5260000228881836, 'confidence': 0.9105256199836731}],
-    [],
-    [],
-    [{'depth': 0.27400001883506775, 'confidence': 0.2727588713169098}],
-    [{'depth': 1.8620001077651978, 'confidence': 0.9919590950012207}],
-    [{'depth': 1.2780001163482666, 'confidence': 0.9998899698257446}],
-    [{'depth': 1.255000114440918, 'confidence': 0.942328691482544}],
-    [{'depth': 1.4800000190734863, 'confidence': 0.9984509944915771}],
-    [{'depth': 1.4940000772476196, 'confidence': 0.9989467263221741}],
-    [{'depth': 1.5190000534057617, 'confidence': 0.9986746311187744}],
-    [],
-    [{'depth': 1.7810001373291016, 'confidence': 0.9949648976325989}],
-    [{'depth': 1.7510000467300415, 'confidence': 0.9739450216293335}, {'depth': 2.5830001831054688, 'confidence': 0.92947918176651}],
-    [{'depth': 1.7610000371932983, 'confidence': 0.9768665432929993}, {'depth': 2.7920000553131104, 'confidence': 0.9095771312713623}],
-    [{'depth': 1.7660000324249268, 'confidence': 0.9749782085418701}, {'depth': 2.5300002098083496, 'confidence': 0.9720422625541687}],
-    [{'depth': 1.787000060081482, 'confidence': 0.9496432542800903}, {'depth': 2.616000175476074, 'confidence': 0.9482998251914978}],
-    [{'depth': 1.7510000467300415, 'confidence': 0.995796799659729}]
+    [{'depth': 1.28, 'confidence': 0.9998899698257446}],
+    [{'depth': 1.28, 'confidence': 0.9998899698257446}],
+    [{'depth': 1.28, 'confidence': 0.9998899698257446}],
+    [{'depth': 1.15, 'confidence': 0.942328691482544}],
+    [{'depth': 1.15, 'confidence': 0.942328691482544}],
+    [{'depth': 1.0, 'confidence': 0.942328691482544}],
+    [{'depth': .5, 'confidence': 0.942328691482544}],
+    [{'depth': .5, 'confidence': 0.942328691482544}],
 ]
 
 data_store = []
@@ -33,7 +24,7 @@ def fake_data_updater():
     while True:
         for data in sample_data:
             data_store = data
-            time.sleep(2)
+            time.sleep(1)
 
 @app.route('/get_people', methods=['GET'])
 def get_people():
